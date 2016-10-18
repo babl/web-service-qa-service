3adadf0fc6a80f9f9416413f72ae87a65fda13a2
Nelson Neves <nelson.s.neves@gmail.com>
2016-10-18 22:20:09 +0100

    deploy latest qa-service with websockets support

--- static/js/qa.js
+++ static/js/qa.js
@@ -1,7 +1,12 @@
 (function () {
   $(window).load(function() {
+    //console.log("window loaded");
     updateRequestHistory();
   });
+  // $(document).ready(function() {
+  //   console.log("document loaded");
+  //   connectWebsocket();
+  // });
 })();
 
 function getMessage(topic, partition, offset) {
@@ -29,12 +34,57 @@ function getError(err) {
   $("#myModal").modal();
 }
 
-function updateRequestHistory() {
+function tableDataAddItem(item) {
   var status_green = "#88CF85"
   var status_red = "#F87070"
   var duration_green = "#48B444"
   var duration_orange = "#F4B27B"
   var duration_red = "#F53636"
+
+  var status_color = item.status == 200 ? status_green : status_red;
+  var duration_color;
+  if (item.duration_ms < 1000) {
+    duration_color = duration_green;
+  } else if (item.duration_ms >= 1000 && item.duration_ms < 1500) {
+    duration_color = duration_orange;
+  } else {
+    duration_color = duration_red;
+  }
+  $("#tableDataHeader").after(
+    "<tbody id=\""+item.rid+"\" class=\"bodycontent\">"+
+      "<tr class=\"trcontent\">"+
+        "<th class=\"text-center\">"+
+          "<a href=\"javascript:void(0)\"><span id=\"icon-"+item.rid+"\" class=\"glyphicon glyphicon-plus\" aria-hidden=\"true\" onclick=\"getRequestDetails("+item.rid+")\"></span></a>"+
+        "</th>"+
+        "<td>"+item.time+"</td>"+
+        "<td>"+item.rid+"</td>"+
+        "<td>"+item.supervisor+"</td>"+
+        "<td>"+item.module+"</td>"+
+        "<td><span style=\"color:"+duration_color+"\">"+item.duration_ms+"</span></td>"+
+        "<td bgcolor=\""+status_color+"\">"+item.status+"</td>"+
+      "</tr>"+
+    "</tbody>");
+}
+
+function tableDataAddInfo(msg) {
+  $("#tableDataHeader").after(
+    "<tbody class=\"bodycontent\">"+
+    "  <tr class=\"trcontent\">"+
+    "    <td colspan=\"8\" class=\"text-center\"><h6>"+msg+"</h6></td>"+
+    "  </tr>"+
+    "</tbody>");
+}
+
+function tableDataAppendCount(count) {
+  $("#tableData").append(
+    "<tbody class=\"bodycontent\">"+
+    "  <tr class=\"trcontent\">"+
+    "    <td colspan=\"8\" class=\"text-center\"><h6>Found "+count+" records</h6></td>"+
+    "  </tr>"+
+    "</tbody>");
+}
+
+function updateRequestHistory() {
   var blocksize = $("#datasize").val()
   var urlRequestHistory = "/api/request/history?blocksize="+blocksize;
   $.getJSON( urlRequestHistory, {
@@ -42,39 +92,12 @@ function updateRequestHistory() {
   })
   .done(function(data) {
     var count = data.length
-    var data_reverse = data.slice(0).reverse();
+    //var data_reverse = data.slice(0).reverse();
     $(".bodycontent").remove()
-    $.each(data_reverse, function(i, item) {
-      var status_color = item.status == 200 ? status_green : status_red;
-      var duration_color;
-      if (item.duration_ms < 1000) {
-        duration_color = duration_green;
-      } else if (item.duration_ms >= 1000 && item.duration_ms < 1500) {
-        duration_color = duration_orange;
-      } else {
-        duration_color = duration_red;
-      }
-      $("#tableData").append(
-        "<tbody id=\""+item.rid+"\" class=\"bodycontent\">"+
-          "<tr class=\"trcontent\">"+
-            "<th class=\"text-center\">"+
-              "<a href=\"javascript:void(0)\"><span id=\"icon-"+item.rid+"\" class=\"glyphicon glyphicon-plus\" aria-hidden=\"true\" onclick=\"getRequestDetails("+item.rid+")\"></span></a>"+
-            "</th>"+
-            "<td>"+item.time+"</td>"+
-            "<td>"+item.rid+"</td>"+
-            "<td>"+item.supervisor+"</td>"+
-            "<td>"+item.module+"</td>"+
-            "<td><span style=\"color:"+duration_color+"\">"+item.duration_ms+"</span></td>"+
-            "<td bgcolor=\""+status_color+"\">"+item.status+"</td>"+
-          "</tr>"+
-        "</tbody>");
+    $.each(data, function(i, item) {
+      tableDataAddItem(item);
     });
-    $("#tableData").append(
-      "<tbody class=\"bodycontent\">"+
-      "  <tr class=\"trcontent\">"+
-      "    <td colspan=\"8\" class=\"text-center\"><h6>Found "+count+" records</h6></td>"+
-      "  </tr>"+
-      "</tbody>");
+    tableDataAppendCount(count);
   });
 }
 
@@ -113,7 +136,7 @@ function getRequestDetails(rid) {
     $.each(data, function(i, item) {
       var msgHidden = 'hidden'
       var errHidden = 'hidden'
-      if (item.topic.length > 0 && item.offset > 0)
+      if (item.topic.length > 0 && (item.partition > 0 || item.offset > 0))
         msgHidden = ''
       if (item.status != 200 && item.status != 0 && item.error.length > 0)
         errHidden = ''
@@ -149,4 +172,46 @@ function getRequestDetails(rid) {
       "        <td colspan=\"8\">"+details+"</td>"+
       "    </tr>")
   });
+}
+
+var websocket;
+function connectWebsocket() {
+
+  function resetWs() {
+    websocket.onmessage = function () {};
+    websocket.onclose = function () {};
+    websocket.onopen = function () {};
+    websocket.close();
+    websocket = null;
+  }
+
+  if (websocket != null) {
+    resetWs();
+    tableDataAddInfo("Websocket connection closed");
+    $("#wsButton").removeClass('active').removeClass('btn-primary');
+    return;
+  }
+
+  if (window["WebSocket"]) {
+      var host = window.location.host;
+      websocket = new WebSocket("ws://" + host + "/ws");
+      websocket.onopen = function(evt) {
+        tableDataAddInfo("Websocket connection opened");
+        $("#wsButton").addClass('active').addClass('btn-primary');
+      }
+      websocket.onclose = function(evt) {
+        tableDataAddInfo("Websocket connection closed");
+        $("#wsButton").removeClass('active').removeClass('btn-primary');
+        resetWs();
+      };
+      websocket.onmessage = function(evt) {
+        var messages = evt.data.split('\n');
+        for (var i = 0; i < messages.length; i++) {
+          var item = JSON.parse(messages[i]);
+          tableDataAddItem(item);
+        }
+      };
+  } else {
+    tableDataAddInfo("Your browser does not support WebSockets");
+  }
 }
